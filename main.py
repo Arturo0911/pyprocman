5b0e8e03b76d87c28c43d65e0e976674ccb3caeb
Arturo0911 <anegreiross@outlook.com>
2023-08-27 12:21:20 -0500

    ok

--- main.py
+++ main.py
@@ -43,20 +43,6 @@ def main_():
 
 def main():
     x = PrettyTable()
-    """
-    x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-
-    x.add_row(["Adelaide", 1295, 1158259, 600.5])
-    x.add_row(["Brisbane", 5905, 1857594, 1146.4])
-    x.add_row(["Darwin", 112, 120900, 1714.7])
-    x.add_row(["Hobart", 1357, 205556, 619.5])
-    x.add_row(["Sydney", 2058, 4336374, 1214.8])
-    x.add_row(["Melbourne", 1566, 3806092, 646.9])
-    x.add_row(["Perth", 5386, 1554769, 869.4])
-
-    print(x)
-    """
-
     x.field_names = ["username", "pid", "name", "cpu_percent", "memory_info"]
     for process in psutil.process_iter(["username", "pid", "name", "cpu_percent", "memory_info"]):
         # print(f"name: {x.info['name']}")
